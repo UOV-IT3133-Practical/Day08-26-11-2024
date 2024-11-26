e134de844fa970b5c4d903f0d840fe9c05aec32f
Nazik <151235734+MohamadNazik@users.noreply.github.com>
2024-11-26 09:48:09 +0000

    simple login system created

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
     const navigate = useNavigate();
-    const aunthenticate = ()=>{
-        navigate('/dashboard')
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const aunthenticate = (e)=>{
+        e.preventDefault();
+        if(userName === "nazik" && password === "N12345"){
+            navigate('/dashboard');  
+        }
+        setError("Username or Password is Incorrect! Please Check")
     }
     return (
       <div>
         <h1>Login Page</h1>
-        <button onClick={()=>aunthenticate()}>Click here to Login</button>
+        <form onSubmit={(e)=>aunthenticate(e)}>
+            <div>
+                <label>Username: </label>
+                <input type="text" name="userName" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+
+        {error && <p>{error}</p>}
       </div>
     );
   }
