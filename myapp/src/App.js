4d05b6087bdfb8a67e48a98db580a485f344bcea
Nazik <151235734+MohamadNazik@users.noreply.github.com>
2024-11-26 08:34:14 +0000

    Routes are created

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,18 @@
+import {BrowserRouter as Router, Routes,Route, Link, NavLink} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<ContactUs/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
